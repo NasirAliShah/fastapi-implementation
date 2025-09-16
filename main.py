c5defff4f4437d55852c4b22fa8510ee6fb13d42
Nasir-Ali-Shah <nasir@troontechnologies.com>
2025-09-16 11:42:58 +0500

    add item and get item

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
 app = FastAPI()
 
+items = []
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/items")
+def create_item(item: str):
+    items.append(item)
+    return items
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int) -> str:
+    item = items[item_id]
+    return item
+    
+@app.get("/items")
+def get_items() -> list:
+    return items
